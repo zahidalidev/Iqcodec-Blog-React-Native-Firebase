d24535ee6a8ae3b727d88a7c7040df4ef705fd1e
zahidalidev <m.zahidalidev@gmail.com>
2021-08-11 11:17:27 +0500

    bug resolved

--- app/screens/PostDetails.js
+++ app/screens/PostDetails.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { View, ScrollView, Text, StatusBar } from 'react-native';
+import { View, ScrollView, Text, StatusBar, Dimensions } from 'react-native';
 import AutoHeightWebView from 'react-native-autoheight-webview'
 import { Appbar } from 'react-native-paper';
 import { RFPercentage } from 'react-native-responsive-fontsize';
 import Colors from '../config/Colors';
 import Constants from 'expo-constants';
+
+const hieght = Dimensions.get('window').height;
 
 function PostDetails(props) {
     const [postDetails, setPostDetail] = useState({})
@@ -39,19 +41,18 @@ function PostDetails(props) {
                     <Text style={{ fontFamily: "sans-serif-medium", color: Colors.white, fontSize: RFPercentage(2.5), marginRight: RFPercentage(4) }} >IQCODEC</Text>
                 </View>
             </Appbar>
-            <ScrollView>
-                <View style={{ width: "90%", marginLeft: "5%", marginTop: RFPercentage(3) }} >
-                    <Text style={{ fontSize: RFPercentage(3), fontWeight: "bold" }} >{postDetails.title}</Text>
-                    <View style={{ flexDirection: "row", marginTop: RFPercentage(0.7) }} >
-                        <Text>By</Text>
-                        <Text style={{ fontWeight: "bold" }} > {postDetails.auther}</Text>
-                        <Text>  -  {postDetails.date}</Text>
-                    </View>
+            <View style={{ width: "90%", marginLeft: "5%", marginTop: RFPercentage(3) }} >
+                <Text style={{ fontSize: RFPercentage(3), fontWeight: "bold" }} >{postDetails.title}</Text>
+                <View style={{ flexDirection: "row", marginTop: RFPercentage(0.7) }} >
+                    <Text>By</Text>
+                    <Text style={{ fontWeight: "bold" }} > {postDetails.auther}</Text>
+                    <Text>  -  {postDetails.date}</Text>
                 </View>
+            </View>
 
-                <AutoHeightWebView style={{ width: "90%", marginTop: RFPercentage(2), marginLeft: "5%" }}
-                    // customScript={`document.body.style.background = 'lightyellow';`}
-                    customStyle={`
+            <AutoHeightWebView style={{ width: "90%", marginTop: RFPercentage(2), marginLeft: "5%" }}
+                // customScript={`document.body.style.background = 'lightyellow';`}
+                customStyle={`
                     p {
                     font-size: 16px;
                     }
@@ -67,18 +68,17 @@ function PostDetails(props) {
                         margin-bottom: 10px
                     }
                 `}
-                    allowsFullscreenVideo={true}
-                    // onSizeUpdated={size => console.log(size.height)}
-                    files={[{
-                        href: 'cssfileaddress',
-                        type: 'text/css',
-                        rel: 'stylesheet'
-                    }]}
-                    source={source}
-                    scalesPageToFit={true}
-                    viewportContent={'width=device-width, user-scalable=no'}
-                />
-            </ScrollView>
+                allowsFullscreenVideo={true}
+                // onSizeUpdated={size => console.log(size.height)}
+                files={[{
+                    href: 'cssfileaddress',
+                    type: 'text/css',
+                    rel: 'stylesheet'
+                }]}
+                source={source}
+                scalesPageToFit={true}
+                viewportContent={'width=device-width, user-scalable=no'}
+            />
         </View>
     );
 }
